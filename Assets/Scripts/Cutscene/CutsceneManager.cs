cf177d22dcbcb1b30c4a661f39cc211322a49202
Vishnu Rajendran <rajendran.vishnu97@gmail.com>
2022-01-30 04:10:05 +0530

    Some stuff

--- Assets/Scripts/Cutscene/CutsceneManager.cs
+++ Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -1,36 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutsceneManager : MonoBehaviour
 {
-    
 
     private static CutsceneManager instance;
     public static CutsceneManager Instance
     {
-        get { 
-            if(instance == null)
+        get {
+            if (instance == null)
+            {
                 instance = FindObjectOfType<CutsceneManager>();
+            }
             return instance; 
         }
     }
 
-#pragma warning disable CS0649 // Field 'CutsceneManager.dialogueWindow' is never assigned to, and will always have its default value null
     [SerializeField] DialogueWindow dialogueWindow;
-#pragma warning restore CS0649 // Field 'CutsceneManager.dialogueWindow' is never assigned to, and will always have its default value null
+    [SerializeField] Image cutSceneBG;
+    [SerializeField] bool isTesting = false;
+    
     public BattleAnimEventComplete OnCutsceneComplete;
+    
+    public void SetImage(Sprite img)
+    {
+        cutSceneBG.gameObject.SetActive(false);
+        if (img == null)
+            return;
+
+        cutSceneBG.gameObject.SetActive(true);
+        cutSceneBG.sprite = img;
+    }
 
     public void LoadCutsceneFor(string charName)
     {
+        OnCutsceneComplete.RemoveListener(OnCutsceneClose);
+        OnCutsceneComplete.AddListener(OnCutsceneClose);
         dialogueWindow.StartDialogue(charName, () =>
         {
             OnCutsceneComplete?.Invoke();
         });
     }
 
+    void OnCutsceneClose()
+    {
+        cutSceneBG.gameObject.SetActive(false);
+    }
+
     private void Start()
     {
-       
+        if(isTesting)
+            LoadCutsceneFor("Test");
     }
 }
