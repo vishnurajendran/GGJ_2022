a1c5f9cdd5b065c4cb343843431484b74a8a4f48
wickedcube <wickedcube@gmail.com>
2022-01-29 14:41:18 +0530

    Card stats and names

--- Assets/Scripts/CardsSO/CardAttributes.cs
+++ Assets/Scripts/CardsSO/CardAttributes.cs
@@ -1,9 +1,13 @@
 using System;
+public enum CardClass {Weight, Liquid, Paper};
+public enum CardType {Hit, Heal};
 
 [Serializable]
 public class CardAttributes
 {
     public string name;
-    public float damage;
+    public float value;
+    public CardClass cardClass;
+    public CardType cardType;
     public int count;
 }
